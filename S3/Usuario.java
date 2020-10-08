8b392ac16ce03d488d97b9182f00e519ec747809
Ivan <ivan.saavedra@ciencias.unam.mx>
2020-10-07 22:55:18 -0500

    upload usuario java

---
+++ S3/Usuario.java
@@ -0,0 +1,76 @@
+package com.restfulwebservices.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Size(min=2, message="El usuario debe tener al menos 2 caracteres")
+	private String usuario;
+	
+	@Size(min=2, message="El password debe tener al menos 8 caracteres")
+	private String password;
+	
+	private Integer tipo_usuaro;
+	
+	public Usuario() {
+		
+	}
+
+	public Usuario(Integer id, @Size(min = 2, message = "El usuario debe tener al menos 2 caracteres") String usuario,
+			@Size(min = 2, message = "El password debe tener al menos 8 caracteres") String password,
+			Integer tipo_usuaro) {
+		super();
+		this.id = id;
+		this.usuario = usuario;
+		this.password = password;
+		this.tipo_usuaro = tipo_usuaro;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getTipo_usuaro() {
+		return tipo_usuaro;
+	}
+
+	public void setTipo_usuaro(Integer tipo_usuaro) {
+		this.tipo_usuaro = tipo_usuaro;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", usuario=" + usuario + ", password=" + password + ", tipo_usuaro=" + tipo_usuaro
+				+ "]";
+	}
+
+}
