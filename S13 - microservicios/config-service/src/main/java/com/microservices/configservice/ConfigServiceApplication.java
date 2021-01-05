ef24766b14e01d5e0bd3f6d8fa76dab21b01755b
Ivan <ivan.saavedra@ciencias.unam.mx>
2021-01-05 15:53:34 -0600

    add config-service

--- S13 - microservicios/config-service/src/main/java/com/microservices/configservice/ConfigServiceApplication.java
+++ S13 - microservicios/config-service/src/main/java/com/microservices/configservice/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.microservicios.configservice;
+package com.microservices.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
