490aeb94cbd4131841f38ab9c6e0a6b128080237
Ivan <ivan.saavedra@ciencias.unam.mx>
2021-01-10 21:21:59 -0600

    add registry-service

---
+++ S14 - microservicios/product-service/src/main/java/com/microservices/product/entity/Category.java
@@ -0,0 +1,17 @@
+package com.microservices.product.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Category {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+
+}
